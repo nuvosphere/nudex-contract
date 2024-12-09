f118a9b874502bcc5a600784f15bbb0b6aeb1ec0
Li-Qing Wang <liqingnz@gmail.com>
2024-12-09 18:12:31 +0800

    feat: implement task pause

--- src/tasks/TaskSubmitterUpgradeable.sol
+++ src/tasks/TaskSubmitterUpgradeable.sol
@@ -14,6 +14,7 @@ contract TaskSubmitterUpgradeable is AccessControlUpgradeable {
 
     uint256 public minDepositAmount;
     uint256 public minWithdrawAmount;
+    mapping(bytes32 pauseType => bool isPaused) public pauseState;
 
     constructor(address _taskManager) {
         taskManager = ITaskManager(_taskManager);
@@ -32,6 +33,10 @@ contract TaskSubmitterUpgradeable is AccessControlUpgradeable {
         minWithdrawAmount = _wAmount;
     }
 
+    function setPauseState(bytes32 _condition, bool _newState) external onlyRole(DEX_ROLE) {
+        pauseState[_condition] = _newState;
+    }
+
     // force submit task by Admin
     function submitTask(
         bytes calldata _context
@@ -42,25 +47,30 @@ contract TaskSubmitterUpgradeable is AccessControlUpgradeable {
     function submitDepositTask(
         address _user,
         uint256 _amount,
-        bytes32 _ticker
+        bytes32 _ticker,
+        uint256 _chainId
     ) external onlyRole(FUNDS_ROLE) returns (uint64) {
+        require(!pauseState[_ticker] && !pauseState[bytes32(_chainId)], "Paused");
         require(_amount >= minDepositAmount, "Below Min Deposit Amount.");
-        return taskManager.submitTask(msg.sender, abi.encodePacked(_user, _amount, _ticker));
+        return
+            taskManager.submitTask(msg.sender, abi.encodePacked(_user, _amount, _ticker, _chainId));
     }
 
     function submitWithdrawTask(
         address _user,
         uint256 _amount,
-        bytes32 _ticker
+        bytes32 _ticker,
+        uint256 _chainId
     ) external onlyRole(FUNDS_ROLE) returns (uint64) {
+        require(!pauseState[_ticker] && !pauseState[bytes32(_chainId)], "Paused");
         require(_amount >= minWithdrawAmount, "Below Min Withdraw Amount.");
         emit INIP20.NIP20TokenEvent_burnb(_user, _ticker, _amount);
-        return taskManager.submitTask(_user, abi.encodePacked(_user, _amount, _ticker));
+        return taskManager.submitTask(_user, abi.encodePacked(_user, _amount, _ticker, _chainId));
     }
 
     function submitAccountCreationTask() external onlyRole(ACCOUNT_ROLE) returns (uint64) {}
 
-    function submitConsolidateTask() external onlyRole(DEX_ROLE) returns (uint64) {}
-
     function submitParticipantUpdateTask() external onlyRole(ACCOUNT_ROLE) returns (uint64) {}
+
+    function submitConsolidateTask() external onlyRole(DEX_ROLE) returns (uint64) {}
 }
