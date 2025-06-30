373e561914b9700e7439cead929cff6cf4d2a6b2
Li-Qing Wang <liqingnz@gmail.com>
2025-06-30 15:52:54 +0800

    fix: check for task state proceeding order

--- src/TaskManagerUpgradeable.sol
+++ src/TaskManagerUpgradeable.sol
@@ -96,7 +96,7 @@ contract TaskManagerUpgradeable is ITaskManager, AccessControlUpgradeable {
         Task storage task;
         for (uint256 i; i < _taskIds.length; ++i) {
             task = tasks[_taskIds[i]];
-            require(task.state != State.Completed && task.state != State.Failed, "Task finalized");
+            require(uint8(_states[i]) > uint8(task.state), "Task finalized");
             task.state = _states[i];
             // TODO: reset dataHash if task failed?
             // if (_state == State.Failed) {
